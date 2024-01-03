d96d5e6b35482d737b007a02f68dbd95134bdcdc
lucas <lucasfrutig0@gmal.com>
2024-01-03 10:25:46 -0300

    fix: astro config for deploy on github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,8 @@ import react from '@astrojs/react'
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
+  site: 'https://lucasfrutig0.github.io/',
+  base: '/pitchdata',
   output: 'server',
   adapter: node({
     mode: 'standalone'
