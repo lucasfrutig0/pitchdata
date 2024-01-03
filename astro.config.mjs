8197ec8f144fb0aa365383e66351edc16b81bbef
lucas <lucasfrutig0@gmal.com>
2024-01-03 12:28:58 -0300

    fix: some configs in astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,6 @@ import cloudflare from '@astrojs/cloudflare'
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  site: 'https://lucasfrutig0.github.io/',
-  base: '/pitchdata',
   output: 'server',
   adapter: cloudflare()
 })
