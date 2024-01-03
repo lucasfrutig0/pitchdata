07e334f53605acccf1a7afe121ff70604fe50755
lucas <lucasfrutig0@gmal.com>
2024-01-03 12:22:35 -0300

    fix: links paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
-import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
-import tailwind from '@astrojs/tailwind';
-import react from '@astrojs/react';
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import react from '@astrojs/react'
 
-import cloudflare from "@astrojs/cloudflare";
+import cloudflare from '@astrojs/cloudflare'
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,4 +11,4 @@ export default defineConfig({
   base: '/pitchdata',
   output: 'server',
   adapter: cloudflare()
-});
+})
--- src/utils/data/leagues.ts
+++ src/utils/data/leagues.ts
@@ -4,27 +4,27 @@ export const leagues = [
     name: 'Premier League',
     code: 'PL',
     slug: 'premier-league',
-    logo: '/images/pl.png'
+    logo: '/pitchdata/images/pl.png'
   },
   {
     id: 2,
     name: 'Serie A',
     code: 'SA',
     slug: 'serie-a',
-    logo: '/images/seriea.png'
+    logo: '/pitchdata/images/seriea.png'
   },
   {
     id: 3,
     name: 'La Liga',
     code: 'LL',
     slug: 'la-liga',
-    logo: '/images/laliga.svg'
+    logo: '/pitchdata/images/laliga.svg'
   },
   {
     id: 4,
     name: 'Champions League',
     code: 'CL',
     slug: 'champions-league',
-    logo: '/images/champions.png'
+    logo: '/pitchdata/images/champions.png'
   }
 ]
