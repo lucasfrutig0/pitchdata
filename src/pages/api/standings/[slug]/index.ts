2cf7986b16fe7b190ebcaa16297e3ea917f68e54
lucas <lucasfrutig0@gmal.com>
2024-01-30 11:20:55 -0300

    fix: league table to astro component

--- src/pages/api/standings/[slug]/index.ts
+++ src/pages/api/standings/[slug]/index.ts
@@ -32,30 +32,30 @@ export const GET: APIRoute = async ({ request }) => {
 
   const data = await fetchData(2023, leagueID?.id as number)
 
-  const transformedData = data?.response[0].league.standings[0].map(
-    (team: any) => {
-      return {
-        position: team.rank,
-        team: team.team.name,
-        logo: team.team.logo,
-        mp: team.all.played,
-        w: team.all.win,
-        d: team.all.draw,
-        l: team.all.lose,
-        gf: team.all.goals.for,
-        ga: team.all.goals.against,
-        gd: team.goalsDiff,
-        pts: team.points,
-        form: team.form,
-        zone: team.description,
-        status: team.status
-      }
-    }
-  )
+  // const transformedData = data?.response[0].league.standings[0].map(
+  //   (team: any) => {
+  //     return {
+  //       position: team.rank,
+  //       team: team.team.name,
+  //       logo: team.team.logo,
+  //       mp: team.all.played,
+  //       w: team.all.win,
+  //       d: team.all.draw,
+  //       l: team.all.lose,
+  //       gf: team.all.goals.for,
+  //       ga: team.all.goals.against,
+  //       gd: team.goalsDiff,
+  //       pts: team.points,
+  //       form: team.form,
+  //       zone: team.description,
+  //       status: team.status
+  //     }
+  //   }
+  // )
 
   return new Response(
     JSON.stringify({
-      league: transformedData
+      league: data?.response[0].league.standings[0]
     }),
     {
       status: 200,
